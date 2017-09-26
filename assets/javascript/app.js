78575b80066c0b36c11172d9d1efdbcd8306da99
BRIAN YACK <bdyack@gmail.com>
2017-09-26 09:00:12 -0500

    omdb

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -149,138 +149,59 @@ $("#user-search-plot").on("click", function() {
     }
 });
 
-// // Listen for Third Button Click ************************API BY BRIAN HERE
-// $("#user-search-clip").on("click", function() {
-//     event.preventDefault();
-//     var nameOfMovieClip = $("#user-input-name").val().trim();
+function displayMovieInfo() {
 
-//     if (nameOfMovieClip === "") {
-//         return;
-//     } else {
-//         console.log(nameOfMovieClip);
+       var movie = $(this).attr("data-name");
+        var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
 
-//         //window.open("https://www.w3schools.com");
-//         // Open New Tab
-//         // Pull Clip from YouTube
-//         // Play Clip in New Tab
-//         $("#user-input-name").val(null);
+       // Creating an AJAX call for the specific movie button being clicked
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
 
-//         function createYouTubeEmbedLink(link) {
-//             link = document.getElementById('link').value;
-//             if (link.charAt(12) == 'y') { //if the 13th character = y (youtube  videos)
-//                 var number = link.substring(32); //key # = from 33rd character on
-//                 var embed = "https://www.youtube.com/embed/" + number; //Add youtube link before key #
-//                 document.getElementById('iframe').src = embed;
-//             } else if (link.charAt(12) == 'o') { //if the 13th character = o (vimeo videos)
-//                 var number = link.substring(18); //key # = from 19th character on
-//                 var embed = "https://player.vimeo.com/video/" + number; //Add vimeo link before key #
-//                 document.getElementById('iframe').src = embed;
-//             } else {}
-//         }
+         // Creating a div to hold the movie
+          var movieDiv = $("<div class='movie'>");
 
-//         function handleAPILoaded() {
-//             console.log('youtube api ready');
-//         }
+         // Storing the rating data
+          var rating = response.Rated;
 
-//         function tplawesome(e, t) { res = e; for (var n = 0; n < t.length; n++) { res = res.replace(/\{\{(.*?)\}\}/g, function(e, r) { return t[n][r] }) } return res }
+         // Creating an element to have the rating displayed
+          var pOne = $("<p>").text("Rating: " + rating);
 
-//         $(function() {
-//             $("form").on("submit", function(e) {
-//                 e.preventDefault();
-//                 console.log('on submit');
-//                 // prepare the request
-//                 var request = gapi.client.youtube.search.list({
-//                     part: "snippet",
-//                     type: "video",
-//                     q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-//                     maxResults: 1,
+         // Displaying the rating
+          movieDiv.append(pOne);
 
-//                 });
-//                 // execute the request
-//                 request.execute(function(response) {
-//                     console.log('test');
-//                     var results = response.result;
-//                     $("#results").html("");
-//                     $.each(results.items, function(index, item) {
-//                         $.get("assets/item.html", function(data) {
-//                             $("#results").append(tplawesome(data, [{ "title": item.snippet.title, "videoid": item.id.videoId }]));
-//                         });
-//                     });
-//                     resetVideoHeight();
-//                 });
-//             });
+         // Storing the release year
+          var released = response.Released;
 
-//             $(window).on("resize", resetVideoHeight);
-//         });
+         // Creating an element to hold the release year
+          var pTwo = $("<p>").text("Released: " + released);
 
-//         function resetVideoHeight() {
-//             $(".video").css("height", $("#results").width() * 9 / 16);
-//         }
+         // Displaying the release year
+          movieDiv.append(pTwo);
 
-//         function init() {
-//             gapi.client.setApiKey("AIzaSyBNszSj_KTlyYE16DegvSVu4Qn6Ivn1Hjwqc");
-//             gapi.client.load("youtube", "v3", function() {
-//                 // yt api is ready
-//             });
-//         }
+         // Storing the plot
+          var plot = response.Plot;
 
-//         var myNodelist = document.getElementsByTagName("LI");
-//         var i;
-//         for (i = 0; i < myNodelist.length; i++) {
-//             var span = document.createElement("SPAN");
-//             var txt = document.createTextNode("\u00D7");
-//             span.className = "close";
-//             span.appendChild(txt);
-//             myNodelist[i].appendChild(span);
-//         }
+         // Creating an element to hold the plot
+          var pThree = $("<p>").text("Plot: " + plot);
 
-//         // Click on a close button to hide the current list item
-//         var close = document.getElementsByClassName("close");
-//         var i;
-//         for (i = 0; i < close.length; i++) {
-//             close[i].onclick = function() {
-//                 var div = this.parentElement;
-//                 div.style.display = "none";
-//             }
-//         }
+         // Appending the plot
+          movieDiv.append(pThree);
 
-//         // Add a "checked" symbol when clicking on a list item
-//         var list = document.querySelector('ul');
-//         list.addEventListener('click', function(ev) {
-//             if (ev.target.tagName === 'LI') {
-//                 ev.target.classList.toggle('checked');
-//             }
-//         }, false);
+         // Retrieving the URL for the image
+          var imgURL = response.Poster;
 
-//         // Create a new list item when clicking on the "Add" button
-//         function newElement() {
-//             var li = document.createElement("li");
-//             var inputValue = document.getElementById("myInput").value;
-//             var t = document.createTextNode(inputValue);
-//             li.appendChild(t);
-//             if (inputValue === '') {
-//                 return;
-//             } else {
-//                 document.getElementById("myUL").appendChild(li);
-//             }
-//             document.getElementById("myInput").value = "";
+         // Creating an element to hold the image
+          var image = $("<img>").attr("src", imgURL);
 
-//             var span = document.createElement("SPAN");
-//             var txt = document.createTextNode("\u00D7");
-//             span.className = "close";
-//             span.appendChild(txt);
-//             li.appendChild(span);
+         // Appending the image
+          movieDiv.append(image);
 
-//             for (i = 0; i < close.length; i++) {
-//                 close[i].onclick = function() {
-//                     var div = this.parentElement;
-//                     div.style.display = "none";
-//                 }
-//             }
-//         }
-
-//     }
-// });
+         // Putting the entire movie above the previous movies
+          $("#movies-view").prepend(movieDiv);
+        });
 
 $("#user-clear").on("click", function() {
      event.preventDefault();
