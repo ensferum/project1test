32eeba01b7ef34fa7b709f61b72bddce4a168c23
BRIAN YACK <bdyack@gmail.com>
2017-09-26 09:21:11 -0500

    testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -133,72 +133,30 @@ database.ref("/movies").on("child_added", function(x) {
 });
 
 // Listen for Second Button Click ************************API BY SHARAD HERE
+
 $("#user-search-plot").on("click", function() {
     event.preventDefault();
     var nameOfMoviePlot = $("#user-input-name").val().trim();
-
     if (nameOfMoviePlot === "") {
         return;
     } else {
         console.log(nameOfMoviePlot);
-        //window.open("https://www.w3schools.com");
-        // Open New Tab
-        // Pull Summary from OMDB
-        // Display Movie Summary in New Tab
+        var queryURL = "http://www.omdbapi.com/?t=" + nameOfMoviePlot + "&y=&plot=short&apikey=40e9cece";
+        // Creating an AJAX call for the specific movie button being clicked
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {    
+         // Storing the plot
+          var plot = response.Plot;
+          // Creating an element to hold the plot
+          var pThree = $("<p>").text("Plot: " + plot);
+          console.log(plot);    
+       });
         $("#user-input-name").val(null);
     }
 });
 
-
-
-//OMDB API
-
-
-function displayMovieInfo() {
-
-        var movie = $(this).attr("#user-search-plot");
-        var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
-
-       // Creating an AJAX call for the specific movie button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response) {
-
-         // Creating a div to hold the movie
-          var movieDiv = $("<div class='movie'>");
-
-         // Storing the rating data
-          var rating = response.Rated;
-
-         // Creating an element to have the rating displayed
-          var pOne = $("<p>").text("Rating: " + rating);
-
-         // Displaying the rating
-          movieDiv.append(pOne);
-
-         // Storing the release year
-          var released = response.Released;
-
-         // Creating an element to hold the release year
-          var pTwo = $("<p>").text("Released: " + released);
-
-         // Displaying the release year
-          movieDiv.append(pTwo);
-
-         // Storing the plot
-          var plot = response.Plot;
-
-         // Creating an element to hold the plot
-          var pThree = $("<p>").text("Plot: " + plot);
-
-         // Appending the plot
-          movieDiv.append(pThree);
-
-
-         // Putting the entire movie above the previous movies
-          $("#movies-view").append(movieDiv);
-        });
 
 $("#user-clear").on("click", function() {
      event.preventDefault();
